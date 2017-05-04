5a3d83f39299b82fae0a73873fc06293120ac4a6
nikhil deore <nikhil.deore@gilbarco.com>
2017-05-04 15:50:37 +0530

    conventions

--- src/main/java/com/gilbarco/challenge/Response.java
+++ src/main/java/com/gilbarco/challenge/Response.java
@@ -3,29 +3,25 @@ package com.gilbarco.challenge;
 public class Response {
 	
 	private String timestamp ;
-	private int counter;
+	private int calls;
 	
 	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
 	}
 	public void setCounter(int counter) {
-		this.counter = counter;
+		this.calls = counter;
 	}
 	public String getTimestamp() {
 		return timestamp;
 	}
 	public int getCounter() {
-		return counter;
+		return calls;
 	}
 	public Response(String timestamp, int counter) {
 		super();
 		this.timestamp = timestamp;
-		this.counter = counter;
+		this.calls = counter;
 	}
-	@Override
-	public String toString() {
-		return "CountAndTime [timestamp=" + timestamp + ", counter=" + counter + ", toString()=" + super.toString()
-				+ "]";
-	}
+	
 
 }
